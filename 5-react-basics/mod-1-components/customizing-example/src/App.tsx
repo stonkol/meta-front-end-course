0e5428af44073276106c9de785eedb05410362a3
stonkol <9.5cc5b@slmails.com>
2025-04-21 18:54:52 +0800

    update

---
+++ 5-react-basics/mod-1-components/customizing-example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + React + TypeScript</h1>
+      <p>Welcome to your new app!</p>
+    </div>
+  );
+}
+
+export default App;
